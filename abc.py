be9e9a782c70403bc6ee85a32993edeb6fe9e71e
MeghanaDuttargi <100281073+MeghanaDuttargi@users.noreply.github.com>
2024-11-08 19:51:40 +0530

    Create abc.py

---
+++ abc.py
@@ -0,0 +1,3 @@
+Write a Python script that takes two integers as input and calculates their GCD using the Euclidean algorithm. 
+   Based on the result, determine whether these numbers are co-prime.
+   If they are co-prime, print a message indicating that they can be used in cryptographic key generation; otherwise, print a message that they are not suitable.
